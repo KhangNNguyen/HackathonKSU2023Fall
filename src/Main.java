88b0b39a689f2302e60e45dbfe307d9ea3e8ea2b
KhangNNguyen <128857346+KhangNNguyen@users.noreply.github.com>
2023-09-15 14:35:46 -0400

    Update to file

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 import java.util.*;
 public class Main {
     public static void main(String[] args){
+
+        //Variables
+
         Scanner scan = new Scanner(System.in);
         int orderNum = 1;
         int totalOrders;
@@ -36,9 +39,34 @@ public class Main {
         double units=0;
         double container=0;
 
+        ArrayList<Double> batteryPallet = new ArrayList<>();
+        ArrayList<Double> rotorsPallet = new ArrayList<>();
+        ArrayList<Double> filtersPallet = new ArrayList<>();
+
+        ArrayList<Double> batteryPalletVolume = new ArrayList<>();
+        ArrayList<Double> rotorsPalletVolume = new ArrayList<>();
+        ArrayList<Double> filtersPalletVolume = new ArrayList<>();
+
+        ArrayList<Double> batteryPalletWeight = new ArrayList<>();
+        ArrayList<Double> rotorsPalletWeight = new ArrayList<>();
+        ArrayList<Double> filtersPalletWeight = new ArrayList<>();
+
+        ArrayList<Double> batteryPalletHeight = new ArrayList<>();
+        ArrayList<Double> rotorsPalletHeight = new ArrayList<>();
+        ArrayList<Double> filtersPalletHeight = new ArrayList<>();
+
+        //Math
+        //batteryPalletWeight = (37 * 100);
+        //rotorPalletWeight = (26.4 * 216);
+        //filtersPalletWeight = (0.65 * 4560;
+
+        //Do-While loop that will take input for # of products
+
+        System.out.print("Enter total number of orders: ");
+        totalOrders = scan.nextInt();
+        System.out.println("Enter the orders from latest delivery to earliest delivery");
         do{
-            System.out.print("Enter total number of orders: ");
-            totalOrders = scan.nextInt();
+
             for (int i = 0; i < totalOrders; i++){
                 System.out.println("Order #"+orderNum);
                 System.out.print("Enter number of car batteries: ");
@@ -99,7 +127,76 @@ public class Main {
                     System.out.println("Your shipment will be optimal when using "+(int)Math.ceil(trucks)+ " box trucks");
                 }
 
+
             }
+
+            for (Integer num: batteryArray){
+                double temp = (double) num / 100;
+                batteryPallet.add(temp);
+            }
+            for (Integer num: rotorArray){
+                double temp = (double) num / 216;
+                rotorsPallet.add(temp);
+            }
+            for (Integer num: filterArray){
+                double temp = (double) num / 456;
+                filtersPallet.add(temp);
+            }
+
+            for (int i = 0; i < totalOrders; i ++){
+                System.out.println("For order "+(i+1)+ ", you will need "+batteryPallet.get(i)+ " pallets for batteries, "+rotorsPallet.get(i) +" pallets for rotors, and "+filtersPallet.get(i)+ " pallets for filters.");
+            }
+
+            // Total Pallet Volume Per Order
+
+            for (int i = 0; i < totalOrders; i ++){
+                double temp = batteryPallet.get(i) * (567.1875 * 100);
+                batteryPalletVolume.add(temp);
+                temp = rotorsPallet.get(i) * (369.54 * 216);
+                rotorsPalletVolume.add(temp);
+                temp = filtersPallet.get(i) * (159.9075 * 456);
+                filtersPalletVolume.add(temp);
+            }
+
+           // for (int i = 0; i < totalOrders; i++){
+             //   System.out.println(batteryPalletVolume.get(i) + " "+rotorsPalletVolume.get(i)+ " " +filtersPalletVolume.get(i));
+            //}
+
+
+            // Pallet weight for the leftovers
+            for (int i = 0; i < totalOrders; i ++){
+                double temp = batteryPallet.get(i) - Math.floor(batteryPallet.get(i));
+                double tempWeight = temp * (37 * 100);
+                batteryPalletWeight.add(tempWeight);
+                temp = rotorsPallet.get(i) - Math.floor(rotorsPallet.get(i));
+                tempWeight = temp * (26.4 * 216);
+                rotorsPalletWeight.add(tempWeight);
+                temp = filtersPallet.get(i) - Math.floor(filtersPallet.get(i));
+                tempWeight = temp * (0.65 * 456);
+                filtersPalletWeight.add(tempWeight);
+            }
+
+            /*for (int i = 0; i < totalOrders; i++){
+                  System.out.println(batteryPalletWeight.get(i) + " "+rotorsPalletWeight.get(i)+ " " +filtersPalletWeight.get(i));
+                } */
+
+
+            // Pallet height for the leftovers
+            for (int i = 0; i < totalOrders; i ++){
+                double temp = batteryPallet.get(i) - Math.floor(batteryPallet.get(i));
+                double tempHeight = temp * (7.5 * 100);
+                batteryPalletHeight.add(tempHeight);
+                temp = rotorsPallet.get(i) - Math.floor(rotorsPallet.get(i));
+                tempHeight = temp * (2.19 * 216);
+                rotorsPalletHeight.add(tempHeight);
+                temp = filtersPallet.get(i) - Math.floor(filtersPallet.get(i));
+                tempHeight = temp * (1.38 * 456);
+                filtersPalletHeight.add(tempHeight);
+            }
+
+            /*for (int i = 0; i < totalOrders; i++){
+                  System.out.println(batteryPalletHeight.get(i) + " "+rotorsPalletHeight.get(i)+ " " +filtersPalletHeight.get(i));
+                }*/
 
 
         } while (orderNum <= totalOrders);
